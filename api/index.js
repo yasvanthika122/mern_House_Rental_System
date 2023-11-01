c55b7d9eb54a2da6e9fad7cc6458b646f9625e9a
Yasv#20041 <yasvathika.21it@kongu.edu>
2023-11-01 21:54:13 +0530

    complete

--- api/index.js
+++ api/index.js
@@ -38,8 +38,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(4000, () => {
-  console.log('Server is running on port  4000!');
+app.listen(5000, () => {
+  console.log('Server is running on port  5000!');
   //console.log('JWT_SECRETE',process.env.JWT_SECRET);
 });
 
